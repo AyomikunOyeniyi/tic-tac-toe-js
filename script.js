fc3af1734910e01d201462ecf33c7079de2a4f4c
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-01-13 01:35:27 -0500

    Initialize gameboard array

---
+++ script.js
@@ -0,0 +1,28 @@
+function Gameboard() {
+    let gameboard = [];
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            gameboard.push('');
+            gameboard[i]
+        }
+    }
+
+    
+
+    return {gameboard};
+};
+
+console.log(Gameboard())
+
+
+function Players() {
+
+};
+
+function Game() {
+
+};
+
+
+
